83f9a0815eb2c84d53074c5a9ae5984cb3838518
naiemislam16 <naiem.islam169643@gmail.com>
2021-03-02 17:34:38 +0600

    a

---
+++ naiem.js
@@ -0,0 +1,22 @@
+alert('the page is not found');
+document.write("page is not found <br> <br> <br><br>");
+document.write('Naiem Islam')
+var x = 10;
+var y = 20;
+var z = x + y;
+document.write(x -= y);
+document.write('</br>');
+document.write(x += y);
+document.write('</br>');
+
+document.write(x *= y);
+document.write('</br>');
+
+document.write(x /= y);
+document.write('</br>');
+
+document.write(x %= y);
+document.write('</br>');
+
+document.write(x -= y);
+document.write('</br>');
